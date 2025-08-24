50f97850d747946fcd6b382a16a92ff1a52a69e8
Vikas Kumar <krvikas1011@gmail.com>
2025-08-24 21:28:09 +0530

    added initial code

---
+++ backend/src/main/java/com/shuviklabs/profilez/ProfilezApplication.java
@@ -0,0 +1,13 @@
+package com.shuviklabs.profilez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilezApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shuviklabs/profilez/ProfilezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuviklabs.profilez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
